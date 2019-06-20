10485c920a9f49f171a87f940c4925bdb66c7532
Chonlatas Tasanapreechachai <chonlatas.t@gmail.com>
2019-06-20 14:24:07 +0700

    can insert data to firedatabase

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomerService } from '../shared/customer.service'
-import { NgForm } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { CustomerService } from "../shared/customer.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"]
 })
 export class CustomerComponent implements OnInit {
-
-  constructor(private customerService: CustomerService) { }
-  submitted = false
+  constructor(private customerService: CustomerService) {}
+  submitted = false;
+  showSuccessMsg = false;
   formControls = this.customerService.form.controls;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
-    this.submitted = true
+    this.submitted = true;
     if (this.customerService.form.valid) {
-
-      this.submitted = false
+      if (this.customerService.form.get("$key").value == null) {
+        this.customerService.insertCustomer(this.customerService.form.value);
+        this.showSuccessMsg = true;
+        setTimeout(() => this.showSuccessMsg=false, 3000);
+      }
+      this.submitted = false;
+      this.customerService.form.reset();
     }
   }
-
 }
--- src/app/shared/customer.service.ts
+++ src/app/shared/customer.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database"
 
 @Injectable({
   providedIn: "root"
 })
 export class CustomerService {
-  constructor() {}
+  constructor( private firebase: AngularFireDatabase) {}
+
+  customerList: AngularFireList<any>;
+
   form = new FormGroup({
     $key: new FormControl(null),
     name: new FormControl('', Validators.required),
     mobile: new FormControl('', [Validators.required, Validators.minLength(10)]),
     email: new FormControl('', [Validators.required, Validators.email])
   });
+
+  getCustomers() {
+    this.customerList = this.firebase.list('customers');
+    return this.customerList.snapshotChanges();
+  }
+
+  insertCustomer(customer) {
+    this.customerList.push({
+      name: customer.name,
+      mobile: customer.mobile,
+      email: customer.email
+    })
+  }
+
 }
